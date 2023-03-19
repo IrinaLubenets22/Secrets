0793faa6bc2d606293062b016c306927793d49ad
Ira Lubenets <ira@MacBook-Air-Ira.local>
2023-03-19 17:20:06 +0200

    step 3 secure - hashing passwords with md5

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -26,7 +26,6 @@ const userSchema = new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model('User', userSchema);
 
@@ -47,7 +46,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
 	const newUser = new User({
 		email: req.body.username,
-		password: req.body.password
+		password: md5(req.body.password)
 	});
 	newUser.save().then(function () {
 		res.render("secrets");
@@ -58,7 +57,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
 	const userName = req.body.username;
-	const password = req.body.password;
+	const password = md5(req.body.password);
 	User.findOne({email: userName}).then(function(founUser){
 		if (founUser) {
 			if (founUser.password === password) {
